ad9280b5a9bd84ca4f57387df0fef8bc707b49cf
Salma Guettiti <79937471+Guettiti-Salma@users.noreply.github.com>
2021-11-17 14:25:25 +0100

    Fixing code

--- Assets/DoorGrabbable.cs
+++ Assets/DoorGrabbable.cs
@@ -1,10 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+//using UnityEngine.SceneManagement;
+
 
 public class DoorGrabbable : OVRGrabbable
 {
     public Transform handler;
+
+
+
     public override void GrabEnd(Vector3 linearvelocity, Vector3 angularvelocity)
     {
         base.GrabEnd(Vector3.zero, Vector3.zero);
@@ -17,6 +22,10 @@ public class DoorGrabbable : OVRGrabbable
     }
     private void update()
     {
+         //float angleAfterDrag = transform.rotation.eulerAngles.z;
+        //if (angleAfterDrag > -57.8f) SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1); 
+
+
         if (Vector3.Distance(handler.position,transform.position) > 0.04f)
         {
             grabbedBy.ForceRelease(this);
